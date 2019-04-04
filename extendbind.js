a956f8fa1a3a596a86ad9a2f768eeb244530611d
nfwyst <nfwyst@gmail.com>
2019-04-04 16:11:46 +0800

    update

--- extendbind.js
+++ extendbind.js
@@ -1,31 +1,35 @@
 class Bind {
   constructor() {
-    Reflect
-      .ownKeys(this.constructor.prototype)
-      .forEach(item => {
-        this[item] = this[item].bind(this);
-      })
+    Bind._init(this);
+  }
+
+  static _super(self) {
+    return Object.getPrototypeOf(self);
+  }
+
+  static _getValue(self, prop, target) {
+    const val = target ? target[prop] : self[prop];
+    return typeof val === 'function' ? val.bind(self) : val;
+  }
+
+  static _init(self) {
+    const { _super } = Bind;
+    let current = _super(self);
+    while (_super(current)) {
+      Reflect.ownKeys(current)
+        .forEach(item => {
+          if (self[item]) {
+            return self[item] = Bind._getValue(self, item);
+          }
+          self[item] = Bind._getValue(current, item, current);
+        })
+      current = _super(current);
+    }
+  }
+
+  static autoBind(self) {
+    return Bind._init(self);
   }
 }
 
-class Base extends Bind {
-  constructor(name) {
-    super();
-    this.doubleName = name.repeat(2);
-  }
-}
-
-class Test extends Base {
-  constructor(name) {
-    super(name);
-    this.name = name;
-  }
-
-  say() {
-    console.log(this.name, this.doubleName);
-  }
-}
-
-const { say } = new Test('thecatblack');
-
-say()
+module.exports = Bind;
